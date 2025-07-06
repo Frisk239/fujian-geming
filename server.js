5124ea2dd135d087897be494d30ae48cd3b29801
余磊 <a2691218460@outlook.com>
2025-07-06 12:46:32 +0800

    修改了主页

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// 中间件
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+// API路由
+app.get('/api/events', (req, res) => {
+    res.sendFile(path.join(__dirname, 'events.json'));
+});
+
+// 启动服务器
+app.listen(port, () => {
+    console.log(`服务器运行在 http://localhost:${port}`);
+    console.log(`请访问 http://localhost:${port}/index.html 查看地图`);
+});
